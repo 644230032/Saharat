76f7f454b2b78f272752b6a4c53cd518e40fffd7
Saharat <96410941+644230032@users.noreply.github.com>
2023-12-15 14:27:22 +0700

    .commit

--- home.py
+++ home.py
@@ -5,3 +5,11 @@ st.title('การทดสอบเขียนเว็บด้วย Python
 st.header('สหรัฐ พลสันต์')
 st.subheader('สาขาวิชาวิทยาการข้อมูล')
 st.markdown("----")
+
+col1, col2 = st.columns(2)
+#col1.write("This is column 1")
+#col2.write("This is column 2")
+with col1:
+    st.image('./pic/Arm.jpg')
+with col2:
+    st.image('./pic/hi.jpg')
