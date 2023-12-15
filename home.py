5a36fa86393bcd163f1d007fd8f54ec9decbdc85
Saharat <96410941+644230032@users.noreply.github.com>
2023-12-15 14:20:33 +0700

    .commit

--- home.py
+++ home.py
@@ -1,3 +1,7 @@
 import streamlit as st
 
 st.header("Saharat Pholsan")
+st.title('การทดสอบเขียนเว็บด้วย Python')
+st.header('ผศ.ดร.ไก้รุ่ง เฮงพระพรหม')
+st.subheader('สาขาวิชาวิทยาการข้อมูล')
+st.markdown("----")
