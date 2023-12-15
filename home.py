9922051512ac95eba3971ddadad42fb4df6e4f0c
Saharat <96410941+644230032@users.noreply.github.com>
2023-12-15 14:39:34 +0700

    .commit

--- home.py
+++ home.py
@@ -13,7 +13,7 @@ with col1:
     st.image('./pic/Arm.jpg')
 with col2:
     st.image('./pic/fl.jpg')
-    
+
     html_1 = """
 <div style="background-color:#52BE80;padding:15px;border-radius:15px 15px 15px 15px;border-style:'solid';border-color:black">
 <center><h5>สถิติข้อมูลดอกไม้</h5></center>
