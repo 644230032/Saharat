36573497cebadbcf8368990781ab06f892df04f5
Saharat <96410941+644230032@users.noreply.github.com>
2023-12-15 14:22:21 +0700

    Update home.py

--- home.py
+++ home.py
@@ -2,6 +2,6 @@ import streamlit as st
 
 st.header("Saharat Pholsan")
 st.title('การทดสอบเขียนเว็บด้วย Python')
-st.header('ผศ.ดร.ไก้รุ่ง เฮงพระพรหม')
+st.header('สหรัฐ พลสันต์')
 st.subheader('สาขาวิชาวิทยาการข้อมูล')
 st.markdown("----")
